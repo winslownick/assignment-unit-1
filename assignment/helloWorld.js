03114c684c9fbd8482790c80edf4eafb6dfb32fd
Nick <winslowknick@gmail.com>
2024-02-19 19:18:37 -0600

    added javascript code, hello world multiple ways.

---
+++ assignment/helloWorld.js
@@ -0,0 +1,26 @@
+console.log('Hello World!');
+
+
+
+const greetings = ['Hello human', 'Hello person', 'Hello world', 'Hello alien'];
+console.log(greetings[2]);
+
+
+
+const hello = 'Hello World!';
+console.log(hello);
+
+
+
+
+const acknowledgement = true
+    if(acknowledgement) {
+     console.log('Hello World!');
+    } else {
+     console.log('Hello outerspace.');
+    }
+
+    //ternary operator
+    acknowledgement ? console.log('Hello World!') : console.log('Hello outerspace');
+
+    
